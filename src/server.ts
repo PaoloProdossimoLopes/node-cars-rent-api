691691afe9b787998b75e887d8a2fa4d5455af03
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2024-01-14 11:31:20 -0300

    refactor: improve routes reuse

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import express from 'express'
 import { categoriesRoutes, } from './routes/categories.routes'
 import { specificationRouter } from './routes/specifications.routes'
+import { routes } from './routes/index'
 
 const port = 3333
 const app = express()
 
 app.use(express.json())
 
-app.use('/categories', categoriesRoutes)
-app.use('/specifications', specificationRouter)
+app.use(routes)
 
 app.listen(port, () => console.log('HTTP server is running on port ' + port))
